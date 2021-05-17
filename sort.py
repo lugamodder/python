6eca031f7e7cc236e4832c43ab3297debb86ed64
lugamodder <lugamodder@gmail.com>
2021-05-17 23:22:36 +0300

    initial

---
+++ sort.py
@@ -0,0 +1,10 @@
+a = [1,4,2,5,3,8,22,54,11,69,7,33,62,71,0,83,99,12,34,21]
+
+def bubble(a):
+    for j in range (0,len(a)):
+         for i in range(0,len(a)-1):
+             if a[i] < a[i+1]:
+                 a[i],a[i+1] = a[i+1],a[i]
+    return a
+ 
+print(bubble(a))
